495d93cc610b9079188109be2ce1e423ec354ceb
fleax <alex.aranda@gmail.com>
2014-05-01 23:21:31 +0200

    Search documents

--- src/main/java/com/github/fleax/shoppinglist/model/ItemBean.java
+++ src/main/java/com/github/fleax/shoppinglist/model/ItemBean.java
@@ -1,44 +1,48 @@
 package com.github.fleax.shoppinglist.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 /**
  * A shopping list item
  * 
  * @author fleax
  * 
  */
+@JsonInclude(Include.NON_NULL)
 public class ItemBean {
 
-	private String id;
+    private String id;
 
-	private String name;
+    private String name;
 
-	/**
-	 * @return the id
-	 */
-	public String getId() {
-		return id;
-	}
+    /**
+     * @return the id
+     */
+    public String getId() {
+	return id;
+    }
 
-	/**
-	 * @param id
-	 *            the id to set
-	 */
-	public void setId(String id) {
-		this.id = id;
-	}
+    /**
+     * @param id
+     *            the id to set
+     */
+    public void setId(String id) {
+	this.id = id;
+    }
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
+    /**
+     * @return the name
+     */
+    public String getName() {
+	return name;
+    }
 
-	/**
-	 * @param name
-	 *            the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+    /**
+     * @param name
+     *            the name to set
+     */
+    public void setName(String name) {
+	this.name = name;
+    }
 }
--- src/main/java/com/github/fleax/shoppinglist/rest/ItemResource.java
+++ src/main/java/com/github/fleax/shoppinglist/rest/ItemResource.java
@@ -19,89 +19,143 @@ import com.github.fleax.shoppinglist.model.ItemBean;
 import com.github.fleax.shoppinglist.utils.Logger;
 import com.google.appengine.api.search.Document;
 import com.google.appengine.api.search.Field;
+import com.google.appengine.api.search.GetRequest;
+import com.google.appengine.api.search.GetResponse;
 import com.google.appengine.api.search.Index;
 import com.google.appengine.api.search.IndexSpec;
 import com.google.appengine.api.search.PutException;
 import com.google.appengine.api.search.ScoredDocument;
 import com.google.appengine.api.search.SearchServiceFactory;
 import com.google.appengine.api.search.StatusCode;
-import com.googlecode.objectify.ObjectifyService;
 
 @Path("/items")
 public class ItemResource {
 
-	@GET
-	public Response list(@Context SecurityContext security) {
-		Principal principal = security.getUserPrincipal();
-		if (principal != null) {
-			return Response.ok(
-					ObjectifyService.ofy().load().type(ItemBean.class)
-							.filter("user", principal.getName()).list())
-					.build();
-
+    @GET
+    @Path("/reset")
+    public Response reset(@Context SecurityContext security) {
+	Principal principal = security.getUserPrincipal();
+	if (principal != null) {
+	    List<String> items = new ArrayList<>();
+	    boolean moreResults = true;
+	    Index index = getIndexForUser(principal.getName());
+	    do {
+		List<String> ids = new ArrayList<>();
+		GetRequest request = GetRequest.newBuilder()
+			.setReturningIdsOnly(true).build();
+		GetResponse<Document> response = index.getRange(request);
+		if (response.getResults().isEmpty()) {
+		    moreResults = false;
 		}
-		return Response.status(Status.UNAUTHORIZED).build();
+		for (Document doc : response) {
+		    ids.add(doc.getId());
+		}
+		index.delete(ids);
+	    } while (moreResults);
+	    return Response.ok(items).build();
 	}
+	return Response.status(Status.UNAUTHORIZED).build();
+    }
 
-	@GET
-	@Path("/+name/{name}")
-	public Response search(@Context SecurityContext security,
-			@PathParam("name") String name) {
-		Principal principal = security.getUserPrincipal();
-		if (principal != null) {
-
-			IndexSpec indexSpec = IndexSpec.newBuilder()
-					.setName(principal.getName()).build();
-			Index index = SearchServiceFactory.getSearchService().getIndex(
-					indexSpec);
-
-			List<ItemBean> items = new ArrayList<>();
-			for (ScoredDocument document : index.search(name)) {
-				ItemBean item = new ItemBean();
-				item.setId(document.getId());
-				item.setName(document.getOnlyField("original").getText());
-				items.add(item);
-			}
-
-			return Response.ok(items).build();
+    @GET
+    public Response list(@Context SecurityContext security) {
+	Principal principal = security.getUserPrincipal();
+	if (principal != null) {
+	    List<ItemBean> items = new ArrayList<>();
+	    boolean moreResults = true;
+	    do {
+		GetRequest request = GetRequest.newBuilder().setLimit(1000)
+			.build();
+		GetResponse<Document> response = getIndexForUser(
+			principal.getName()).getRange(request);
+		if (response.getResults().isEmpty()) {
+		    moreResults = false;
 		}
-		return Response.status(Status.UNAUTHORIZED).build();
+		for (Document doc : response) {
+		    items.add(documentToBean(doc));
+		}
+	    } while (moreResults);
+	    return Response.ok(items).build();
 	}
+	return Response.status(Status.UNAUTHORIZED).build();
+    }
 
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response create(@Context SecurityContext security,
-			final ItemBean item) {
-		Principal principal = security.getUserPrincipal();
-		if (principal != null) {
-			createDocument(principal.getName(), item);
-			return Response.ok(item).build();
-		}
-		return Response.status(Status.UNAUTHORIZED).build();
+    @GET
+    @Path("/+name/{name}")
+    public Response search(@Context SecurityContext security,
+	    @PathParam("name") String name) {
+	Principal principal = security.getUserPrincipal();
+	if (principal != null) {
+	    Index index = getIndexForUser(principal.getName());
+
+	    List<ItemBean> items = new ArrayList<>();
+	    for (ScoredDocument document : index.search(name)) {
+		items.add(documentToBean(document));
+	    }
+
+	    return Response.ok(items).build();
 	}
+	return Response.status(Status.UNAUTHORIZED).build();
+    }
 
-	private void createDocument(String user, ItemBean item) {
-		Document.Builder builder = Document.newBuilder();
-		builder.addField(Field.newBuilder().setName("original")
-				.setText(item.getName()).build());
-
-		for (int i = 2; i < item.getName().length(); i++) {
-			builder.addField(Field.newBuilder().setName("tokens")
-					.setText(item.getName().subSequence(0, i).toString())
-					.build());
-		}
-
-		IndexSpec indexSpec = IndexSpec.newBuilder().setName(user).build();
-		Index index = SearchServiceFactory.getSearchService().getIndex(
-				indexSpec);
-
-		try {
-			index.put(builder.build());
-		} catch (PutException e) {
-			if (StatusCode.TRANSIENT_ERROR.equals(e.getOperationResult()
-					.getCode())) {
-				Logger.exception(e);
-			}
-		}
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response create(@Context SecurityContext security,
+	    final ItemBean item) {
+	Principal principal = security.getUserPrincipal();
+	if (principal != null) {
+	    createDocument(principal.getName(), item);
+	    return Response.ok(item).build();
 	}
+	return Response.status(Status.UNAUTHORIZED).build();
+    }
+
+    private void createDocument(String user, ItemBean item) {
+	try {
+	    getIndexForUser(user).put(beanToDocument(item));
+	} catch (PutException e) {
+	    if (StatusCode.TRANSIENT_ERROR.equals(e.getOperationResult()
+		    .getCode())) {
+		Logger.exception(e);
+	    }
+	}
+    }
+
+    /**
+     * @param user
+     * @return index for user
+     */
+    private Index getIndexForUser(String user) {
+	return SearchServiceFactory.getSearchService().getIndex(
+		IndexSpec.newBuilder().setName(user).build());
+    }
+
+    private List<ItemBean> getItems(List<Document> results) {
+	List<ItemBean> result = new ArrayList<>();
+	for (Document document : results) {
+	    result.add(documentToBean(document));
+	}
+	return result;
+    }
+
+    private ItemBean documentToBean(Document document) {
+	ItemBean itemBean = new ItemBean();
+	itemBean.setId(document.getId());
+	itemBean.setName(document.getOnlyField("original").getText());
+	return itemBean;
+    }
+
+    private Document beanToDocument(ItemBean item) {
+	Document.Builder builder = Document.newBuilder();
+	builder.addField(Field.newBuilder().setName("original")
+		.setText(item.getName()).build());
+
+	for (int i = 2; i < item.getName().length(); i++) {
+	    builder.addField(Field.newBuilder().setName("tokens")
+		    .setText(item.getName().subSequence(0, i).toString())
+		    .build());
+	}
+	return builder.build();
+    }
+
 }
